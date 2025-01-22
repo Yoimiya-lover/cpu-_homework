428f63a86e758f7009dbcc62176a05371c3d49eb
jikai <jikai@insta360.com>
2025-01-22 15:30:06 +0800

    origin homework

---
+++ include/Matrix_Mul_Algorithm.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <xmmintrin.h>
+#include "WorkerThread.h"
+
+namespace MMA{
+    using Elemtype = float;
+    void MatrixMulOrign(Elemtype* A,Elemtype* B,Elemtype* C,const int M,const int N,const int K){
+        
+    }
+
+
+
+}
