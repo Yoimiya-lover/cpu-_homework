f1ad652234b6c0df90416861b923d0f232ffb89c
jikai <jikai@insta360.com>
2025-01-22 18:39:58 +0800

    完成线程库

--- include/WorkerThread.h
+++ include/WorkerThread.h
@@ -10,13 +10,51 @@ namespace MMA{
             std::thread worker;
 
         public:
-            template <typename Func, typename... Args>
-            WorkerThread(Func&& func, Args&&... args) {
-            worker = std::move(std::thread(std::forward<Func>(func), std::forward<Args>(args)...));
+            /* typename... Args是不定参数 
+                std::forward<Func>(func)完美转发
+            */
+            template<typename Func, typename... Args>
+            WorkerThread(Func&& func,Args&&... args){
+                worker = std::move(std::thread(std::forward<Func>(func),std::forward<Args>(args)...));
+            }
+
+            WorkerThread& operator=(WorkerThread&& other) noexcept{
+                if(this != &other)
+                {
+                    worker = std::move(other.worker);
+                    std::cout << "Copy Assigning: " << std::endl;
+                }
+                return *this;
+            }
+
+
+            WorkerThread& operator=(WorkerThread&& other) noexcept {
+            if (this != &other) {
+                worker = std::move(other.worker);
+                std::cout << "Copy Assigning: " << std::endl;
+                }
+            return *this;
             }
 
             void join(){
-                
+                if(worker.joinable()){
+                    worker.join();
+                }
+
+            }
+
+            bool joinable(){
+                return worker.joinable();
+            }
+
+            ~WorkerThread(){
+                if(worker.joinable()){
+                    worker.join();
+                }
+            }
+
+            void detach(){
+                worker.detach();
             }
     };
 
